db8d498c570fd6ea038cd4c6eea1fea824385bbd
Alexey Slizkov <elexunix@gmail.com>
2022-01-26 17:43:44 +0300

    Update field4by4-calc-ones.cpp: remove redundant input prompt

--- reachability-256/field4by4-calc-ones.cpp
+++ reachability-256/field4by4-calc-ones.cpp
@@ -8,11 +8,9 @@ int main() {
   for (uint32_t u = 0; u < 256; ++u) {
     ones_cnt[u] = __builtin_popcount(u);
   }
+  int layer_sum;
   unsigned char *layer;
-  while (true) {
-    int layer_sum;
-    std::cout << "layer: ";
-    std::cin >> layer_sum;
+  while (std::cin >> layer_sum) {
     layer = new unsigned char[index_dp[16][layer_sum] / 8 + 1];
     read_arr_from_disk(index_dp[16][layer_sum] / 8 + 1, layer, "arrays4by4-256/ulayer" + itos(layer_sum, 4) + ".dat");
     long long cnt_ones = 0;
